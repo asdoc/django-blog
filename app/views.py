17162e574022980789a795bc4e4b4fe1216a91a0
asdoc <asdocode@gmail.com>
2014-09-25 17:26:39 +0530

    Added functionality to skip uploads

--- app/views.py
+++ app/views.py
@@ -8,12 +8,15 @@ from bson.objectid import ObjectId
 
 def save(name,content,file_store):
 	client = pymongo.MongoClient('localhost',27017).new
-	fs = gridfs.GridFS( client )
-
-	fileID = fs.put( file_store )
-	file_entry = { 'name':file_store.name, 'file_id':fileID }
-	new_entry = { 'author':name , 'content':content , 'hits':0 , 'file':file_entry }
-	client.new.insert(new_entry)
+	if file_store != None:
+		fs = gridfs.GridFS( client )
+		fileID = fs.put( file_store )
+		file_entry = { 'name':file_store.name, 'file_id':fileID }
+		new_entry = { 'author':name , 'content':content , 'hits':0 , 'file':file_entry }
+		client.new.insert(new_entry)
+	else:
+		new_entry = { 'author':name , 'content':content , 'hits':0 }		
+		client.new.insert(new_entry)
 	
 def get():
 	blogs = []
@@ -26,12 +29,13 @@ def get():
 		blog_data['author']=i['author']
 		blog_data['content']=i['content']
 		blog_data['hits']=str(i['hits'])
+		'''
 		blog_data['file_path']= (str(i['file']['file_id'])+"/"+i['file']['name'])
-
 		tmp = os.system("mkdir app/static/"+str(i['file']['file_id']))
 		to_write = open( "app/static/"+(str(i['file']['file_id'])+"/"+i['file']['name']) , 'w' )
 		to_write.write(fs.get(i['file']['file_id']).read())
 		to_write.close()
+		'''
 
 		blogs.append(blog_data)
 	return blogs
@@ -50,13 +54,13 @@ def get_blog(id_blog):
 		blog_data['content']=i['content']
 		blog_data['hits']=str(i['hits'])
 
-		blog_data['file_name']=i['file']['name']
-		blog_data['file_path']= (str(i['file']['file_id'])+"/"+i['file']['name'])
-
-		tmp = os.system("mkdir app/static/"+str(i['file']['file_id']))
-		to_write = open( "app/static/"+(str(i['file']['file_id'])+"/"+i['file']['name']) , 'w' )
-		to_write.write(fs.get(i['file']['file_id']).read())
-		to_write.close()
+		if 'file' in i:
+			blog_data['file_name']=i['file']['name']
+			blog_data['file_path']= (str(i['file']['file_id'])+"/"+i['file']['name'])
+			tmp = os.system("mkdir app/static/"+str(i['file']['file_id']))
+			to_write = open( "app/static/"+(str(i['file']['file_id'])+"/"+i['file']['name']) , 'w' )
+			to_write.write(fs.get(i['file']['file_id']).read())
+			to_write.close()
 
 		blogs.append(blog_data)
 	return blogs
@@ -85,19 +89,29 @@ def add(request):
 	if request.method=="POST":
 		author = request.POST['author']
 		content = request.POST['content']
-		file_store = request.FILES['file']
-		save(author,content,file_store)
+		if 'file' in request.FILES:
+			file_store = request.FILES['file']
+			save(author,content,file_store)
+		else:
+			save(author,content,None)
+		
 		return HttpResponse("<html><h1>Saved successfully</h1><br/>Go to <a href='/'>home</a></html>")	
 	return render(request,'add.html')
 	
 def blog(request,id_blog):
+	blogs=[]
 	blog_data = get_blog(id_blog)
-	print blog_data[0]['file_name']
-	blog_ext = blog_data[0]['file_name'].split('.')
-	blog_ext = blog_ext[len(blog_ext)-1]
-	if blog_ext=='mp4':
-		blog_data[0]['video']=True
-	else:
-		blog_data[0]['video']=False
-	context = { 'blogs': blog_data }
+	for i in blog_data:
+		if 'file_name' in i:
+			blog_ext = i['file_name'].split('.')
+			blog_ext = blog_ext[len(blog_ext)-1]
+			if blog_ext=='mp4':
+				i['video']=True
+			else:
+				i['video']=False
+			i['file_present']=True
+		else:
+			i['file_present']=False
+		blogs.append(i)
+	context = { 'blogs': blogs }
 	return render(request,'blog.html',context)
